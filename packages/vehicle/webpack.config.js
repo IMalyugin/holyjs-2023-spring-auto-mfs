d9e931a566755c541263087d93b1c9e1783ec4e2
imalyugin <imalyugin@beeline.ru>
2022-09-29 15:25:55 +0300

    Commit #3: Move shared webpack configuration data to new shared package

--- packages/vehicle/webpack.config.js
+++ packages/vehicle/webpack.config.js
@@ -1,79 +1,15 @@
-const path = require('path');
 const { ModuleFederationPlugin } = require('webpack').container;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { config, useModuleFederationPlugin } = require('@beemfs/af-shared');
 
 module.exports = {
-  mode: 'development',
+  ...config,
   entry: './',
-  output: {
-    filename: 'static/[name].js',
-    publicPath: 'auto',
-    assetModuleFilename: `static/assets/[name].[hash][ext][query]`,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react'],
-        },
-      }, {
-        test: /\.pcss$/,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: {
-                localIdentName: '[folder]_[local]_[hash:base64:4]',
-              },
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  ['postcss-preset-env', {
-                    stage: 1,
-                  }],
-                ],
-              },
-            },
-          },
-        ]
-      }, {
-        test: /\.(jpg|png)$/,
-        type: 'asset/resource'
-      }
-    ],
-  },
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   template: './server/views/index.html',
-    // }),
-    new ModuleFederationPlugin({
+    useModuleFederationPlugin({
       name: 'vehicle',
-      filename: 'remoteEntry.js',
       exposes: {
         '.': '.'
       },
-      shared: {
-        react: { singleton: true },
-        'react-dom': { singleton: true },
-      },
-    })
+    }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    port: 8081,
-  }
 };
