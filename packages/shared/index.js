c421dc37acf1e7bb83ad3e008ea7ac267ff720f3
imalyugin <imalyugin@beeline.ru>
2023-05-15 11:33:47 +0300

    Commit #11: Detect mf via pattern

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -1,8 +1,7 @@
 const { config } = require('./src/webpackConfig.js');
-const { useModuleFederationPlugin, getMfPort } = require('./src/webpackUtils.js');
+const { useModuleFederationPlugin } = require('./src/webpackUtils.js');
 
 module.exports = {
   config,
-  getMfPort,
   useModuleFederationPlugin,
 };
